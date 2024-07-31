58de2eeba8b5597ce7953d6c0e5d06d3db4c35c6
04082008Bruno <161615256+04082008Bruno@users.noreply.github.com>
2024-07-31 11:19:20 -0300

    tesao

---
+++ codigo.js
@@ -0,0 +1,11 @@
+let NASC;
+let ANO;
+let IDADE;
+
+resultado = ANO - NASC;
+
+NASC = parseInt(prompt(" Você nasceu quando ? "));
+ANO  = parseInt(prompt(" Em que ano estamos ? "));
+IDADE= ANO - NASC;
+
+alert(" Você tem " + IDADE );
