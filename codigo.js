8c534e0b6be6cf2020eb9036f24c3cfd1f9dc07c
04082008Bruno <161615256+04082008Bruno@users.noreply.github.com>
2024-08-01 12:14:51 -0300

    melhorado

--- codigo.js
+++ codigo.js
@@ -1,32 +1,66 @@
-let NOME;
-let NASC;
-let ANO;
-let IDADE;
-let DOG;
-let IDADEDOG;
-let qualCalculo; 
+    //variaveis
+    /* let anoNascimento;
+     let anoAtual;
+     let resultado;
+     let idadeCachorro;
+     let nome;
+     let qualCalculo;
 
-resultado = ANO - NASC;
-NOME = prompt("Qual seu nome?")
-NASC = parseInt(prompt("Você nasceu quando?  "));
-ANO =  parseInt(prompt("Em que ano estamos?  "));
-IDADE = ANO - NASC;
 
-alert(NOME + "  Você tem " + IDADE );
+     //entrada de dados pelo usuario
+     nome = prompt("Qual seu nome?");
+     anoNacimento = parseInt(prompt("Qual o ano de nacimento?"));
+     anoAtual = parseInt(prompt("Qual o ano atual?"));
 
-DOG = parseInt(prompt("O seu cachorro nasceu quando? "));
-ANO = parseInt(prompt("Em que ano estamos ?"));
-IDADEDOG = (ANO - DOG) * 7;
-alert("O seu cachorro tem " + Math.floor(IDADEDOG) );
+   //primeiro desafio
+   resultado = anoAtual - anoNacimento;
 
-qualCalculo = parseInt("Digite uma idade humana e 2 para idade canina ")
 
-if(qualCalculo == 1){
-    alert( NOME + "A idade é"+ IDADE)
-}
-else if(qualCalculo ==2){
-    alert(NOME + ",a idade em anos caninos é" + IDADEDOG)
-}
-else {
-    alert(NOME + ", por favor digite um número válido")
+   //segundo desafio
+   idadeCachorro = resultado * 7;
+
+   
+   //terceiro desafio
+
+   //quarto desafio
+   qualCalculo = parseInt(prompt("Digite 1 pra idade Humana ou 2 para idade Canina"));
+
+   //mostrar resultado
+   if(qualCalculo == 1){
+      alert(nome + ", a idade é: " + resultado);
+   }
+   else if(qualCalculo == 2){
+      alert(nome + ", aidade em anos caninos é: " + idadeCachorro);
+   } else {
+      alert(nome + ", por favor digite um numero valido.");
+   }*/
+
+  
+  
+  
+   //desafio ultimato
+   // fazer uma calculadora que verefica quantos pontos um estudante
+   //precisa para passar de ano no ultimo trimestre
+
+   //dica: o estudante precisa informar as notas do primeiro e segundo trimestre 
+   //com esses dadosa calculadora precisa informar quantos pontos ele precisa para passar 
+   //de ano no terceiro trimestre. se o estudante ja tiver passado no segundo trimestre,
+   //madar uma messsagem de parabens!
+
+   //utilizar como média o valor 6.0
+
+
+let nota1;
+let nota2;
+let resultado;
+
+nota1 = parseInt(prompt("qual a nota do 1trim?"));
+nota2 = parseInt(prompt("qual a nota do 2trim?"));
+
+resultado = 180 - (nota1+nota2);
+
+if(resultado > 0){
+    alert("ainda precisa de " + resultado + "para passar.");
+} else {
+    alert("Parabéns! Você já etá aprovado!");
 }
